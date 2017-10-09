2e2f53ac317e868d9fb9d75eae88864317e4dabe
khai96_ <hvksmr1996@gmail.com>
2017-10-09 07:45:01 +0700

    Basic sections

--- src/lib/components/body.php
+++ src/lib/components/body.php
@@ -1,12 +1,15 @@
 <?php
 require_once __DIR__ . '/base.php';
+require_once __DIR__ . '/header-section.php';
+require_once __DIR__ . '/main-section.php';
+require_once __DIR__ . '/footer-section.php';
 
 class Body implements Component {
   public function render(): Component {
     return HTMLElement::create('body', array(
-      HTMLElement::create('h1', array(
-        'Hello, World!!'
-      ))
+      new HeaderSection(),
+      new MainSection(),
+      new FooterSection()
     ));
   }
 }
