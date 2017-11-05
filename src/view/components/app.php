b80f9d7362ec93315346cc9e3a530ea0f44733aa
khai96_ <hvksmr1996@gmail.com>
2017-11-05 14:10:16 +0700

    Don't make a seperate component when it is not reused

--- src/view/components/app.php
+++ src/view/components/app.php
@@ -1,13 +1,19 @@
 <?php
 require_once __DIR__ . '/base.php';
-require_once __DIR__ . '/head-section.php';
-require_once __DIR__ . '/body-section.php';
 
 class App implements Component {
   public function render(): Component {
     return HtmlElement::create('html', array(
-      new HeadSection(),
-      new BodySection()
+      'lang' => 'en',
+      HtmlElement::create('head', array(
+        HtmlElement::create('meta', array('charset' => 'utf-8')),
+        HtmlElement::create('title', 'Hello, World!!')
+      )),
+      HtmlElement::create('body', array(
+        HtmlElement::create('header'),
+        HtmlElement::create('main'),
+        HtmlElement::create('footer')
+      ))
     ));
   }
 }
--- src/view/components/footer-section.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/base.php';
-
-class FooterSection implements Component {
-  public function render(): Component {
-    return HtmlElement::create('footer');
-  }
-}
-?>
