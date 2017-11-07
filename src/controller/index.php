789cd08f236cd1daf8777dacec36f6b34552421f
khai96_ <hvksmr1996@gmail.com>
2017-11-07 10:05:39 +0700

    Validate $_GET['theme']

--- src/controller/index.php
+++ src/controller/index.php
@@ -5,6 +5,20 @@ require_once __DIR__ . '/../view/index.php';
 function main(): string {
   $urlQuery = new UrlQuery($_GET);
 
+  $themeName = $urlQuery->getDefault('theme', 'light');
+  $themeColorSet = null;
+
+  switch($themeName) {
+    case 'light':
+      $themeColorSet = new LightThemeColors();
+      break;
+    case 'dark':
+      $themeColorSet = new DarkThemeColors();
+      break;
+    default:
+      $urlQuery->set('theme', 'light')->redirect();
+  }
+
   $data = array(
     'url-query' => $urlQuery
   );
