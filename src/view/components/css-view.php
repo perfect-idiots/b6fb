d563776214458f086c16a4f205456c4e4806b330
khai96_ <hvksmr1996@gmail.com>
2017-11-07 23:42:09 +0700

    Check for unsupplied variables

--- src/view/components/css-view.php
+++ src/view/components/css-view.php
@@ -23,6 +23,14 @@ class CssView implements Component {
       );
     }
 
+    $regres = preg_match('/\[\[[a-zA-Z\-]+\]\]/', $css, $matches);
+    if ($regres === false) {
+      throw new Exception('An error occurred when counting remain CSS unsupplied variables');
+    } else if ($regres !== 0) {
+      $list = implode(', ', $matches);
+      throw new Error("Some variables are not supplied: $list");
+    }
+
     return HtmlElement::create('style', array(
       'type' => 'text/css',
       new UnescapedText($css),
