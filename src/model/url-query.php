c64d5d64b0216f349e3f2b59243c363d85f1113a
khai96_ <hvksmr1996@gmail.com>
2017-11-07 09:58:20 +0700

    Create UrlQuery::redirect

--- src/model/url-query.php
+++ src/model/url-query.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/../lib/utils.php';
+require_once __DIR__ . '/../lib/render.php';
+require_once __DIR__ . '/../view/components/redirect-page.php';
 
 class UrlQuery extends RawDataContainer {
   private $prefix, $separator;
@@ -10,6 +12,14 @@ class UrlQuery extends RawDataContainer {
 
   public function getUrlQuery(): string {
     return '?' . http_build_query($this->getData());
+  }
+
+  public function redirect(string $prefix = ''): void {
+    $location = $prefix . $this->getUrlQuery();
+    header("Location: $location");
+    $renderer = new Renderer(true);
+    $page = new RedirectPage($location);
+    echo $renderer->render($page);
   }
 }
 ?>
