c168080c0144dd9414e47db92416dbe72247cf90
khai96_ <hvksmr1996@gmail.com>
2017-11-07 08:03:19 +0700

    Move YamlObjectLoader to lib/yaml.php

--- src/model/predefined.php
+++ src/model/predefined.php
@@ -2,12 +2,6 @@
 require_once __DIR__ . '/../lib/yaml.php';
 require_once __DIR__ . '/../lib/utils.php';
 
-class YamlObjectLoader extends LazyLoadedDataContainer {
-  protected function load(): array {
-    return spyc_load_file($this->param);
-  }
-}
-
 class PredefinedGames extends YamlObjectLoader {
   public function __construct() {
     parent::__construct(__DIR__ . '/predefined/games.yaml');
