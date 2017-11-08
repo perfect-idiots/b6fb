78da80a9e0e00f5ac2a5a07e21d490b850087b26
khai96_ <hvksmr1996@gmail.com>
2017-11-09 02:11:20 +0700

    Create hidden-input

---
+++ src/view/components/hidden-input.php
@@ -0,0 +1,39 @@
+<?php
+require_once __DIR__ . '/base.php';
+require_once __DIR__ . '/../../lib/utils.php';
+
+class HiddenInput implements Component {
+  private $name, $value, $attr;
+
+  public function __construct(string $name, string $value, array $attr = array()) {
+    $this->name = $name;
+    $this->value = $value;
+    $this->attr = $attr;
+  }
+
+  public function render(): Component {
+    return HtmlElement::create('input', array_merge(
+      array(
+        'type' => 'hidden',
+        'hidden' => true,
+        'name' => $this->name,
+        'value' => $this->value,
+      ),
+      $this->attr
+    ));
+  }
+}
+
+class HiddenInputSet extends RawDataContainer implements Component {
+  public function render(): Component {
+    $data = $this->getData();
+    $children = array();
+
+    foreach($data as $name => $value) {
+      array_push($children, new HiddenInput($name, $value));
+    }
+
+    return HtmlElement::create('div', $children);
+  }
+}
+?>
