14122d13404c3481adfd5a35b9c8e963d6251077
khai96_ <hvksmr1996@gmail.com>
2017-11-08 13:42:26 +0700

    Create ErrorPage

--- src/view/index.php
+++ src/view/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/../lib/utils.php';
 require_once __DIR__ . '/../lib/render.php';
+require_once __DIR__ . '/components/base.php';
 require_once __DIR__ . '/components/app.php';
+require_once __DIR__ . '/components/meta-element.php';
 
 abstract class Page extends RawDataContainer {
   abstract protected function component(): Component;
@@ -17,6 +19,33 @@ abstract class Page extends RawDataContainer {
 class MainPage extends Page {
   protected function component(): Component {
     return new App($this->getData());
+  }
+}
+
+class ErrorPage extends Page {
+  protected function component(): Component {
+    $status = $this->get('status');
+    $message = "[TODO] explain $status";
+    http_response_code($status);
+
+    return HtmlElement::create('html', array(
+      'lang' => 'en',
+      HtmlElement::create('head', array(
+        new CharsetMetaElement('utf-8'),
+        new NamedMetaElement('status', $status),
+        HtmlElement::create('title', $message),
+      )),
+      HtmlElement::create('body', array(
+        HtmlElement::nested(array('header', 'h1', 'code'), array(
+          (string) $status
+        )),
+        HtmlElement::nested(array('main', 'output'), $message),
+      )),
+    ));
+  }
+
+  static public function status(int $status): self {
+    return new static(array('status' => $status));
   }
 }
 ?>
