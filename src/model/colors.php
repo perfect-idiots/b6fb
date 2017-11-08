de7a4e5ea4ba1fd70916891423426b796b3cf0b8
khai96_ <hvksmr1996@gmail.com>
2017-11-08 21:07:24 +0700

    Use php to define colors

--- src/model/colors.php
+++ src/model/colors.php
@@ -1,17 +1,17 @@
 <?php
-require_once __DIR__ . '/../lib/yaml.php';
+require_once __DIR__ . '/../lib/utils.php';
 
-abstract class ThemeColorSet extends FixedYamlObjectLoader {}
+abstract class ThemeColorSet extends FixedArrayLoader {}
 
 class LightThemeColors extends ThemeColorSet {
   static protected function filename(): string {
-    return __DIR__ . '/colors/light.yaml';
+    return __DIR__ . '/colors/light.php';
   }
 }
 
 class DarkThemeColors extends ThemeColorSet {
   static protected function filename(): string {
-    return __DIR__ . '/colors/dark.yaml';
+    return __DIR__ . '/colors/dark.php';
   }
 }
 ?>
