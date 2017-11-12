8cb60b8e2ffabd223d418100b17838673e12d87c
khai96_ <hvksmr1996@gmail.com>
2017-11-12 21:54:25 +0700

    SearchBox background is always darker than HeaderSection

--- src/model/colors/light.php
+++ src/model/colors/light.php
@@ -4,7 +4,7 @@ return [
   'background-color' => 'white',
   'header-text-color' => 'white',
   'header-background-color' => '#66f',
-  'search-box-background-color' => '#88f',
+  'search-box-background-color' => '#3030de',
   'shadow-color' => 'rgba(100, 100, 100, 0.5)',
 ];
 ?>
