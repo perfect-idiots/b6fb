9a56470df604fadaf81caa08922fce5882111ae3
khai96_ <hvksmr1996@gmail.com>
2017-11-12 15:40:30 +0700

    Rename to header-text-color

--- src/model/colors/dark.php
+++ src/model/colors/dark.php
@@ -2,7 +2,7 @@
 return [
   'text-color' => 'white',
   'background-color' => '#222',
-  'logo-text-color' => 'white',
+  'header-text-color' => 'white',
   'header-background-color' => '#333',
   'search-box-background-color' => '#444',
   'input-focused-box-shadow-color' => 'rgba(127, 128, 129, 0.5)',
