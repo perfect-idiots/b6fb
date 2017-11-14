d690a958aa76d286f34f04d78c7217e6ee9c5a69
khai96_ <hvksmr1996@gmail.com>
2017-11-14 07:46:56 +0700

    Remove redundant test line
    Ungit check new changes by default

--- src/view/components/main-section.php
+++ src/view/components/main-section.php
@@ -9,7 +9,6 @@ class MainSection extends RawDataContainer implements Component {
       'id' => 'main-section',
       "Current Page:",
       HtmlElement::nested(['i', 'u', 'code'], $page),
-      HtmlElement::emmetBottom('a-b-c>d-e-f>g-h-i', 'hello'),
     ]);
   }
 }
