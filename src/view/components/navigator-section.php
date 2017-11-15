76acef46d6f812e2a59b191ddfe1b4bf6de19958
khai96_ <hvksmr1996@gmail.com>
2017-11-15 12:57:53 +0700

    Mark link to points to displaying page
    As .current-page

--- src/view/components/navigator-section.php
+++ src/view/components/navigator-section.php
@@ -4,15 +4,19 @@ require_once __DIR__ . '/../../lib/utils.php';
 
 class NavigatorSection extends RawDataContainer implements Component {
   public function render(): Component {
+    $currentpage = $this->get('page');
     $subpagetmpls = $this->get('subpages');
 
     $subpageitems = array_map(
-      function ($tmpl) {
+      function ($tmpl) use($currentpage) {
+        $page = $tmpl['page'];
+
         return HtmlElement::emmetDepth(
-          "li#to-{$tmpl['page']}>a",
+          "li#to-$page>a",
           [
             [
               'dataset' => $tmpl,
+              'classes' => $currentpage === $page ? ['current-page'] : [],
             ],
             [
               'href' => $tmpl['href'],
