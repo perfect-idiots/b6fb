5c02beb5cfaa00abbee099a2a11e78f3769f11d1
cuong8321 <cuong8321@gmail.com>
2017-11-18 09:16:44 +0700

    Admin Login Stylesheet

--- src/view/components/admin-user-interface.php
+++ src/view/components/admin-user-interface.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/base.php';
 require_once __DIR__ . '/meta-element.php';
 require_once __DIR__ . '/css-view.php';
+require_once __DIR__ . '/logo.php';
 require_once __DIR__ . '/../../lib/utils.php';
 
 class AdminUserInterface extends RawDataContainer implements Component {
@@ -14,15 +15,22 @@ class AdminUserInterface extends RawDataContainer implements Component {
         HtmlElement::create('title', 'Administration'),
         CssView::fromFile(__DIR__ . '/../../resources/admin.css'),
       ]),
-      HtmlElement::create('body', [
-        HtmlElement::emmetBottom('header>h1', 'Đăng nhập'),
-        HtmlElement::create('main', [
-          new LoginForm([
-            'hidden-values' => [
-              'page' => 'admin',
-            ],
+      HtmlElement::emmetBottom('body>#page.aligner', [
+        HtmlElement::emmetTop('.top-aligned.aligned-item', []),
+        HtmlElement::emmetTop('.middle-aligned.aligned-item', [
+          HtmlElement::create('header', Logo::instance($this->getData())),
+          HtmlElement::emmetBottom('section#main-section', [
+            HtmlElement::emmetTop('h1#login-title', 'Đăng nhập'),
+            HtmlElement::create('main', [
+              new LoginForm([
+                'hidden-values' => [
+                  'page' => 'admin',
+                ],
+              ]),
+            ]),
           ]),
         ]),
+        HtmlElement::emmetTop('.bottom-aligned.aligned-item', []),
       ]),
     ]);
   }
