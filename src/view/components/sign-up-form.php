a6c344fc750a53927ef0e800af8d41439201e43a
khai96_ <hvksmr1996@gmail.com>
2017-11-18 16:41:01 +0700

    Create sign up page

---
+++ src/view/components/sign-up-form.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/base.php';
+require_once __DIR__ . '/labeled-input.php';
+require_once __DIR__ . '/text-button.php';
+require_once __DIR__ . '/../../lib/utils.php';
+
+class SignUpForm extends RawDataContainer implements Component {
+  public function render(): Component {
+    return HtmlElement::create('form', [
+      HtmlElement::emmetTop('.input-container', [
+        PlainLabeledInput::text('fullname', 'Họ và Tên'),
+        PlainLabeledInput::text('username', 'Tên tài khoản'),
+        SecretLabeledInput::text('password', 'Mật khẩu'),
+        SecretLabeledInput::text('re-password', 'Nhập lại mật khẩu'),
+      ]),
+      HtmlElement::emmetTop('.button-container', [
+        new PrimaryButton([
+          'type' => 'submit',
+          'Tạo tài khoản',
+        ]),
+        new PrimaryButton([
+          'type' => 'clear',
+          'Xóa',
+        ]),
+      ]),
+    ]);
+  }
+}
+?>
