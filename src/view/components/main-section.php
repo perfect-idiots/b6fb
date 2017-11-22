a9ab5ac3c92fe2ee4a93e8e7be22b5169d59f9dd
khai96_ <hvksmr1996@gmail.com>
2017-11-22 14:15:40 +0700

    Create MainContent

--- src/view/components/main-section.php
+++ src/view/components/main-section.php
@@ -9,23 +9,42 @@ class MainSection extends RawDataContainer implements Component {
     $self = $this;
     $data = $this->getData();
     $page = $data['page'];
-    $gamelist = $data['game-manager']->list();
 
     return HtmlElement::create('main', [
       'id' => 'main-section',
-      HtmlElement::emmetTop('#game-menu', array_map(
-        function (array $info) use($self) {
-          [$id, $name] = $info;
-
-          return new GameItem($self->assign([
-            'game-id' => $id,
-            'game-name' => $name,
-          ])->getData());
-        },
-        $gamelist
-      )),
+      new MainContent($this->getData()),
       new FooterSection(),
     ]);
+  }
+}
+
+class MainContent extends RawDataContainer implements Component {
+  public function render(): Component {
+    $page = $this->get('page');
+
+    switch ($page) {
+      case 'index':
+        return new GameMenu($this->getData());
+    }
+  }
+}
+
+class GameMenu extends RawDataContainer implements Component {
+  public function render(): Component {
+    $self = $this;
+    $gamelist = $this->get('game-manager')->list();
+
+    return HtmlElement::emmetTop('#game-menu', array_map(
+      function (array $info) use($self) {
+        [$id, $name] = $info;
+
+        return new GameItem($self->assign([
+          'game-id' => $id,
+          'game-name' => $name,
+        ])->getData());
+      },
+      $gamelist
+    ));
   }
 }
 ?>
