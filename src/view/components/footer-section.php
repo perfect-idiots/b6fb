2d9e42cc18cd30c584a7b3fddfcdba3e7a6fa1b3
khai96_ <hvksmr1996@gmail.com>
2017-11-22 09:02:25 +0700

    Use © sign

--- src/view/components/footer-section.php
+++ src/view/components/footer-section.php
@@ -5,7 +5,7 @@ class FooterSection implements Component {
   public function render(): Component {
     return HtmlElement::create('footer', [
       'id' => 'main-footer',
-      HtmlElement::emmetTop('.copyright', 'Bản quyền (c) 2017 b6fb dev'),
+      HtmlElement::emmetTop('.copyright', 'Bản quyền © 2017 b6fb dev'),
       HtmlElement::emmetBottom('.about>a', [
         'href' => 'https://github.com/perfect-idiots',
         'Về chúng tôi',
