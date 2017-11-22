9b5fe6bf246907e73e67445a7ce19c186d6432eb
khai96_ <hvksmr1996@gmail.com>
2017-11-22 10:03:20 +0700

    Correct delete-user

--- src/controller/delete-user.php
+++ src/controller/delete-user.php
@@ -2,8 +2,25 @@
 require_once __DIR__ . '/security.php';
 
 class DeleteUser extends LoginDoubleChecker {
-  public function delete(string $query): int {
+  static protected function requiredFieldSchema(): array {
+    return array_merge(parent::requiredFieldSchema(), [
+      'username' => 'string',
+    ]);
+  }
+
+  public function checkPermission(): bool {
+    $login = $this->get('login');
+    return $login->username() === $this->get('username') || $login->isAdmin();
+  }
+
+  public function delete(string $query): void {
     $this->verify();
+
+    $this
+      ->get('db-query-set')
+      ->get('delete-users')
+      ->executeOnce()
+    ;
   }
 }
 ?>
