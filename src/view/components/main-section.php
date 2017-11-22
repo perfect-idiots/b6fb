7cbb50f18137fe97e59cf9da81d6264056eeaded
khai96_ <hvksmr1996@gmail.com>
2017-11-22 13:12:03 +0700

    Display game menu

--- src/view/components/main-section.php
+++ src/view/components/main-section.php
@@ -1,15 +1,29 @@
 <?php
 require_once __DIR__ . '/base.php';
+require_once __DIR__ . '/game-item.php';
 require_once __DIR__ . '/footer-section.php';
+require_once __DIR__ . '/../../lib/utils.php';
 
 class MainSection extends RawDataContainer implements Component {
   public function render(): Component {
+    $self = $this;
     $data = $this->getData();
     $page = $data['page'];
+    $gamelist = $data['game-manager']->list();
+
     return HtmlElement::create('main', [
       'id' => 'main-section',
-      "Current Page:",
-      HtmlElement::emmetBottom('i>u>code', $page),
+      HtmlElement::emmetTop('#game-menu', array_map(
+        function (array $info) use($self) {
+          [$id, $name] = $info;
+
+          return new GameItem($self->assign([
+            'game-id' => $id,
+            'game-name' => $name,
+          ])->getData());
+        },
+        $gamelist
+      )),
       new FooterSection(),
     ]);
   }
