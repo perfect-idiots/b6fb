fa1aabeaf4200c0e197f0e7e489d2ad89b519f82
khai96_ <hvksmr1996@gmail.com>
2017-11-28 15:27:53 +0700

    Forgot prefix 'game-'

--- src/view/components/game-item.php
+++ src/view/components/game-item.php
@@ -12,7 +12,7 @@ class GameItem extends RawDataContainer implements Component {
   public function render(): Component {
     $urlQuery = $this->get('url-query');
     $id = $this->get('game-id');
-    $description = $this->getDefault('description', '');
+    $description = $this->getDefault('game-description', '');
 
     return HtmlElement::create('a', [
       'href' => $urlQuery->assign([
