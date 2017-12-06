ba92f4676ddb6938ced439186f6dd077b6183c57
khai96_ <hvksmr1996@gmail.com>
2017-12-06 11:17:38 +0700

    Favourite button

--- src/resources/scripts/script.js
+++ src/resources/scripts/script.js
@@ -15,6 +15,11 @@ function ajax (query) {
   const profileButton = document.getElementById('profile-button')
   const profileSetting = document.getElementById('profile-setting')
   const navHidingButton = document.getElementById('nav-hiding-button')
+  const player = document.querySelector('.x-component--player')
+
+  const isFavourite = () => player.classList.contains('favourite')
+  const addFavourite = () => player.classList.add('favourite')
+  const removeFavourite = () => player.classList.remove('favourite')
 
   profileButton && profileButton.addEventListener('click', () => {
     profileSetting.hidden = !profileSetting.hidden
@@ -35,5 +40,11 @@ function ajax (query) {
     const toggleFavButton = document.createElement('button')
     toggleFavButton.classList.add('toggle-favourite')
     container.appendChild(toggleFavButton)
+
+    toggleFavButton.addEventListener('click', async function ajaxFav () {
+      const key = isFavourite() ? 'userDeleteFavourite' : 'userAddFavourite'
+      const response = await ajax({[key]: player.dataset.gameId})
+      response.payload[key] ? addFavourite() : removeFavourite()
+    }, false)
   })(document.querySelector('.x-component--player .control'))
 })(window)
