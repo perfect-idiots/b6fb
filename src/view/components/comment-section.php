ba0cb449a57505c748bd99167a1e8a7455b1f08f
khai96_ <hvksmr1996@gmail.com>
2017-12-08 22:47:26 +0700

    No needs of game-id nor game-name

--- src/view/components/comment-section.php
+++ src/view/components/comment-section.php
@@ -4,22 +4,7 @@ require_once __DIR__ . '/../../lib/utils.php';
 
 class CommentSection extends RawDataContainer implements Component {
   public function render(): Component {
-    $id = $this->get('game-id');
-    $name = $this->get('game-name');
-
-    return HtmlElement::create('article', [
-      'dataset' => [
-        'game-id' => $id,
-        'game-name' => $name,
-      ],
-    ]);
-  }
-
-  static protected function requiredFieldSchema(): array {
-    return array_merge(parent::requiredFieldSchema(), [
-      'game-id' => 'string',
-      'game-name' => 'string',
-    ]);
+    return HtmlElement::create('article');
   }
 }
 ?>
