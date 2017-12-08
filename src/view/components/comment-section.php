04a4d4eab45549ab33ac1cf5037265fa9961b114
khai96_ <hvksmr1996@gmail.com>
2017-12-08 22:58:35 +0700

    Display comment sections

--- src/view/components/comment-section.php
+++ src/view/components/comment-section.php
@@ -1,10 +1,30 @@
 <?php
 require_once __DIR__ . '/base.php';
+require_once __DIR__ . '/comment.php';
 require_once __DIR__ . '/../../lib/utils.php';
 
 class CommentSection extends RawDataContainer implements Component {
   public function render(): Component {
-    return HtmlElement::create('article');
+    $self = $this;
+    $id = $this->getDefault('game-id', '');
+    $list = $this->get('comment-manager')->listByGame($id, true);
+    $groups = $list['groups'];
+
+    return HtmlElement::create('article', array_map(
+      function (array $thread) use($self) {
+        return new CommentThreadViewer($self->assign([
+          'top' => $thread['top'],
+          'replies' => $thread['replies'],
+        ])->getData());
+      },
+      $groups
+    ));
+  }
+
+  static protected function requiredFieldSchema(): array {
+    return array_merge(parent::requiredFieldSchema(), [
+      'comment-manager' => 'CommentManager',
+    ]);
   }
 }
 ?>
